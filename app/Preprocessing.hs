444f0be939242d5f667a3f1a6b83489931d9eb74
SoraTagami <loslonbervene.1923@icloud.com>
2022-10-06 16:19:59 +0900

    draft-of-rnng (#2)

--- app/Preprocessing.hs
+++ app/Preprocessing.hs
@@ -1,6 +1,7 @@
-module Main where
+module Preprocessing where
 
-import PTB 
+import PTB
+import Util (configLoad, Config(..))
 import Options.Applicative
 import qualified Data.Text as T          --text
 import qualified Data.Text.IO as T       --text
@@ -8,39 +9,55 @@ import System.FilePath.Posix (takeBaseName) --filepath
 import Text.Directory (checkFile, getFileList) --nlp-tools
 import System.Directory (doesDirectoryExist, doesFileExist, listDirectory) --directory
 
+{-
+  PennTreeBankのデータを前処理する
+-}
 
-newtype Preprocess = Preprocess
+newtype PreprocessingOpt = PreprocessingOpt
   {
     path :: FilePath
   } deriving (Show)
 
-preprocess :: Parser Preprocess
-preprocess = Preprocess
+preprocess :: Parser PreprocessingOpt
+preprocess = PreprocessingOpt
   <$> strOption
   ( long "path"
   <> short 'p' 
   )
 
-opts :: ParserInfo Preprocess
+opts :: ParserInfo PreprocessingOpt
 opts = info (preprocess <**> helper)
   ( fullDesc
-  <> progDesc "Load Penn Treebank Data." )
+  <> progDesc "Path of WSJ PennTreeBank data" )
 
 listFiles :: String -> IO [String]
 listFiles p = do
   isFile <- doesFileExist p
   if isFile then return [p] else getFileList "mrg" p
 
-main :: IO()
-main = do
-  options <- execParser opts
-  let directoryPath = path options
-  filePaths <- listFiles directoryPath -- 指定されたディレクトリ以下のファイルを取得
+saveActionData :: [FilePath] -> FilePath -> IO()
+saveActionData dirsPath outputPath = do
+  -- 指定されたディレクトリ以下のファイルを取得
+  filePaths <- fmap concat $ traverse id $ fmap listFiles dirsPath 
+  --readmeは除外
   cfgTreess <- mapM parsePTBfile $ filter (\f -> takeBaseName f /= "readme") filePaths
   let cfgTrees = concat cfgTreess
   let parsedCfgTrees = filter (not . isErr) $ cfgTrees
   mapM_ (putStrLn . show) $ filter isErr $ cfgTrees
-  let cfgActionData = traverseCFGs parsedCfgTrees
-  saveActionsToBinary "data/actions" cfgActionData
-  content <- loadActionsFromBinary "data/actions"
+  let rnngSentence = traverseCFGs parsedCfgTrees
+  saveActionsToBinary outputPath rnngSentence
+  -- content <- loadActionsFromBinary outputPath
+  return ()
+
+main :: IO()
+main = do
+  options <- execParser opts
+  config <- configLoad
+  let wsjDirPath = path options
+      trainingDataDirs = fmap (wsjDirPath ++) ["02/", "03/", "04/", "05/", "06/", "07/", "08/", "09/", "10/", "11/", "12/", "13/", "14/", "15/", "16/", "17/", "18/", "19/", "20/", "21/"]
+      validationDataDirs = fmap (wsjDirPath ++) ["24/"]
+      evaluationDataDirs = fmap (wsjDirPath ++) ["23/"]
+  -- saveActionData trainingDataDirs $ trainingDataPath config
+  saveActionData validationDataDirs $ getValidationDataPath config
+  saveActionData evaluationDataDirs $ getEvaluationDataPath config
   return ()
